6ca24dca9510d951d111b8bf9ad057f7d59c9c21
Snehalkhairnar9 <L00163434@student.lyit.ie>
2021-10-18 16:45:29 +0100

    python code

---
+++ os.py
@@ -0,0 +1,23 @@
+# File          : pythonproject.py
+# Author        : Snehal Khairnar
+# Version       : v1.0
+# Created Date  : 08/10/2021
+# Description   : Testing the functionality of List, Tuples
+# Licensing     : Snehal Khairnar, LYIT
+# ----------------------------------
+import platform
+
+p = platform.uname()
+def os_info():
+
+    print("machine :", p.machine)
+    print("system :", p.system)
+    print("node :", p.node)
+    print("version :", p.version)
+    print("processor :", p.processor)
+    print("release :", p.release)
+
+if "__name__" == "__main__":
+   print("details are as below")
+
+os_info()
